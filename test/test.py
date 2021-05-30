a65054af5ea99474cd6cde528786a317406ff431
xiabee <xiabee@foxmail.com>
2021-05-30 14:55:58 +0800

    update front-end

--- test/test.py
+++
@@ -1,6 +0,0 @@
-import requests
-url = "http://localhost:8545"
-
-data = '{"jsonrpc":"2.0","method":"eth_getTransactionByBlockNumberAndIndex","params":["0xc","0x0"],"id":18}'
-r = requests.post(url,data)
-print(r.text)
